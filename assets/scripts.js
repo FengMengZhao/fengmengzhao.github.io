03e7c79787222c4e1d0dfaf11096009902a3d456
FengMengZhao <fengmengzhao@gmail.com>
2018-04-23 14:47:17 +0800

    add back to top link

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,31 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
+
 })
+/* back to top */
+$('body').prepend('<a href="#" class="back-to-top">Back to Top</a>');
+
+var amountScrolled = 300;
+
+$(window).scroll(function() {
+  if ( $(window).scrollTop() > amountScrolled  ) {
+            //$('a.back-to-top').fadeIn('slow');
+            $('a.back-to-top').show();
+                
+  } else {
+            //$('a.back-to-top').fadeOut('slow');
+            $('a.back-to-top').hide();
+                
+  }
+
+});
+
+$('a.back-to-top').click(function() {
+  $('html, body').animate({
+            scrollTop: 0
+                    
+  }, 700);
+    return false;
+
+});
+
