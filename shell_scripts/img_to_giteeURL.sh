6a1752103d4a76a82becd41f0149ae3e0d2ee0a1
fengmengzhao <arefmzhao@163.com>
2022-01-04 12:01:18 +0800

    修改脚本

--- shell_scripts/img_to_giteeURL.sh
+++ shell_scripts/img_to_giteeURL.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-sudo cp _posts/2021-06-25-docker-handbook-2021.markdown weixin/
+if [ -z "$1" ]; then
+  echo "需要输入参数：bash shell_scripts/img_to_giteeURL.sh 2021-12-20-who-is-responsible-for-my-last-5years-hardworking-but-pay-less.markdown，这里参数是Markdown文章，前面不能加路径。"
+  exit
+fi
 
-#sed -i 's/![](img/![](https:\/\/gitee.com\/fengmengzhao\/fengmengzhao.github.io\/raw\/master\/img/g' 
-sudo sed -i 's/!\[\](\/img/!\[\](https:\/\/gitee.com\/fengmengzhao\/fengmengzhao.github.io\/raw\/master\/img/g' weixin/2021-06-25-docker-handbook-2021.markdown
+/bin/cp _posts/$1 weixin/
+
+#sed -i 's/!\[\](\/img/!\[\](https:\/\/gitee.com\/fengmengzhao\/fengmengzhao.github.io\/raw\/master\/img/g' weixin/$1
+sed -i 's/\/img\/posts\//https:\/\/gitee.com\/fengmengzhao\/fengmengzhao.github.io\/raw\/master\/img\/posts\//g' weixin/$1
